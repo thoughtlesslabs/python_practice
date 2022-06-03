999747d0a1867702f2c43761a0712dab6cd7e4db
John Dietrich <john@thoughtlesslabs.com>
2022-06-03 15:36:59 -0400

    Better formatting

--- 99_bottles.py
+++ 99_bottles.py
@@ -1,14 +1,15 @@
 for i in range(99, 0, -1):
-    bottle_grammar = f"{i} bottles"
-    last_line = f"{i-1} bottles"
     if i == 2:
         last_line = f"{i-1} bottle"
     elif i == 1:
         bottle_grammar = f"{i} bottle"
         last_line = "No more bottles"
+    else:
+        bottle_grammar = f"{i} bottles"
+        last_line = f"{i-1} bottles"
 
     print(bottle_grammar + ' of beer on the wall')
-    print(bottle_grammar + ' bottles of beer!')
+    print(bottle_grammar + ' of beer!')
     print("Take one down, pass it around.")
     print(last_line + " of beer on the wall")
     print("**********************************************")
