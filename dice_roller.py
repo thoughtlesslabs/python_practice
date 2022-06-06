719b25435bf37809e16f135385975416e9fa4dd4
John Dietrich <john@thoughtlesslabs.com>
2022-06-06 09:45:14 -0400

    Simple dice rolling game

---
+++ dice_roller.py
@@ -0,0 +1,37 @@
+import random
+
+
+def player_input():
+    # Ask user how many dice
+    num_dice = int(input("how many dice? "))
+    # Ask user how many sides are on the dice
+    dice_side = 21
+
+    while dice_side > 20:
+        dice_side = int(input("how many sides? "))
+
+    return num_dice, dice_side
+
+
+player_choice = player_input()
+
+while True:
+    rolls = "| "
+
+    for i in range(0, player_choice[0]):
+        roll_die = random.randint(1, player_choice[1])
+        rolls += f"{roll_die} |"
+
+    print(rolls)
+
+    play_again = input("Would you like to play again (y/n)? ")
+
+    if play_again == "y":
+        same_rolls = input("Would you like to change your dice? ")
+        if same_rolls == 'y':
+            player_choice = player_input()
+        continue
+    else:
+        break
+
+print("Thanks for playing!")
